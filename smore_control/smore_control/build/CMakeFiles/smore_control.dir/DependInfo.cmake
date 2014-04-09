b1f1b3630f62ab08d8cfbec0dc5e21f5cd43e9bc
Daniel <daniel@bee10-Ubuntu.(none)>
2014-04-09 15:27:50 -0400

    first commit?

---
+++ smore_control/smore_control/build/CMakeFiles/smore_control.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniel/Desktop/modular_robots/my_code/smore_control/smore_control/main.cpp" "/home/daniel/Desktop/modular_robots/my_code/smore_control/smore_control/build/CMakeFiles/smore_control.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/daniel/Desktop/modular_robots/my_code/smore_control/smore_control/build/msgs/CMakeFiles/command_message.dir/DependInfo.cmake"
+  "/home/daniel/Desktop/modular_robots/my_code/smore_control/smore_control/build/msgs/CMakeFiles/collision_message_plus.dir/DependInfo.cmake"
+  )
