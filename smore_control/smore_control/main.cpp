9890d2bffa81b169458e22e152ed1685b4b15a89
Daniel Lee <daniel@danielLee-Ubuntu12.04>
2014-06-09 21:23:10 -0400

    GUI edited. Now the program/gui should be able to handle multiple modules w/ separate commands, and stuff. The sensory data is not yet separated yet.
    TODO: Test w/ multiple modules and multiple sensory inputs.

--- smore_control/smore_control/main.cpp
+++ smore_control/smore_control/main.cpp
@@ -14,18 +14,46 @@
 #include "smorescontrollerwindow.h"
 #include <QtGui>
 
+struct SMOREControlCommand
+{
+	float velLeftWheel, velRightWheel;
+	float angle1, angle2;
+	std::string topicName;
+	bool doRotate;
+	bool doSwipeUD;
+	bool movingUp;
+	
+	SMOREControlCommand()
+	{
+		velLeftWheel = 0;
+		velRightWheel = 0;
+		angle1 = 0;
+		angle2 = 0;
+		doRotate = false;
+		doSwipeUD = false;
+		movingUp = false;
+		topicName = "";
+	}
+};
+
 // global variables
-boost::shared_ptr<google::protobuf::Message> g_echoMsg;
 bool foundNameIndices = false;
 int poseNameIndex = -1;
 int lidarPoseNameIndex = -1;
-GLViewer* glViewer;
+GLViewer *glViewer;
 
 gazebo::math::Pose robotPose, lidarPose;
 gazebo::transport::PublisherPtr pub;
+gazebo::transport::PublisherPtr worldMessagePublisher;
+
+gazebo::transport::NodePtr node;
 
 double leftWheel = 0, rightWheel = 0, joint1 = 0, joint2 = 0;
 bool doSwippingUD = false, doRotate = false;
+std::string currentlySelectedTopicName;
+std::map<std::string, SMOREControlCommand> topicNameToCommand;
+
+SmoresControllerWindow *gui;
 
 typedef const boost::shared_ptr<const command_message::msgs::WorldMessage> WorldMessagePtr;
 
@@ -70,29 +98,130 @@ void poseCallback(ConstPosesStampedPtr &_msg) {
       lidarPose = gazebo::msgs::Convert(_msg->pose(lidarPoseNameIndex));
       
       glViewer->SetLidarPose(lidarPose + robotPose);
-      
-//       std::cout << "Robot" << std::endl << (gazebo::msgs::Convert(robotPose).DebugString()) << std::endl;
-//       std::cout << "LIDAR" << std::endl << (gazebo::msgs::Convert(lidarPose).DebugString()) << std::endl;
-//       std::cout << "Robot + LIDAR" << std::endl << (gazebo::msgs::Convert(lidarPose + robotPose).DebugString()) << std::endl;
     }
 }
 
-void lidarCallback(ConstLaserScanStampedPtr &msg) {
+void lidarCallback(ConstLaserScanStampedPtr &msg) 
+{
   msg->scan().ranges().data();
   glViewer->SetLidarPoints((double*)(msg->scan().ranges().data()), msg->scan().ranges_size(), msg->scan().angle_min(), msg->scan().angle_max(), msg->scan().angle_step(),
 			   msg->scan().range_min(), msg->scan().range_max());
-  
 }
 
 void SMORESWorldStatusCallback(WorldMessagePtr &msg)
 {
 	if(msg->messagetype() == 11)
 	{
+        std::vector<std::string> names;
 		for(int i = 0; i < msg->stringmessages_size(); i++)
 		{
-			std::cout << "Name: " << msg->stringmessages(i) << std::endl;
+			names.push_back(msg->stringmessages(i));
+			bool foundSameTopicName = false;
+			
+			for(std::map<std::string, SMOREControlCommand>::iterator it = topicNameToCommand.begin(); it != topicNameToCommand.end(); ++it)
+			{
+				if(it->first.compare(msg->stringmessages(i)) == 0)
+				{
+					foundSameTopicName = true;
+					break;
+				}
+			}
+			if(!foundSameTopicName)
+			{
+				topicNameToCommand[msg->stringmessages(i)] = SMOREControlCommand();
+			}
 		}
+        std::cout << "Set SMORE NAMES" << std::endl;
+        gui->SetGUIComponents(SmoresControllerWindow::SMORES_NAMES_LIST_VIEW, names);
 	}
+}
+
+void GUIButtonCallback(SmoresControllerWindow::GUIBtnType type)
+{
+    std::cout << "Got Message" << std::endl;
+	currentlySelectedTopicName = gui->GetCurrentSelectedTopic();
+	
+    if(type == SmoresControllerWindow::GETMODULES)
+    {
+        command_message::msgs::WorldMessage worldMsg;
+        worldMsg.set_messagetype(1);
+        worldMessagePublisher->Publish(worldMsg);
+    }
+    else if(type == SmoresControllerWindow::VEL_UP)
+    {
+		topicNameToCommand[currentlySelectedTopicName].velLeftWheel += 0.1;
+		topicNameToCommand[currentlySelectedTopicName].velRightWheel += 0.1;
+//         leftWheel += 0.1;
+//         rightWheel += 0.1;
+    }
+    else if(type == SmoresControllerWindow::VEL_DOWN)
+    {
+        topicNameToCommand[currentlySelectedTopicName].velLeftWheel -= 0.1;
+        topicNameToCommand[currentlySelectedTopicName].velRightWheel -= 0.1;
+    }
+    else if(type == SmoresControllerWindow::VEL_RIGHT)
+    {
+        topicNameToCommand[currentlySelectedTopicName].velLeftWheel += 0.1;
+        topicNameToCommand[currentlySelectedTopicName].velRightWheel -= 0.1;
+    }
+    else if(type == SmoresControllerWindow::VEL_LEFT)
+    {
+        topicNameToCommand[currentlySelectedTopicName].velLeftWheel -= 0.1;
+        topicNameToCommand[currentlySelectedTopicName].velRightWheel += 0.1;
+    }
+    else if(type == SmoresControllerWindow::STOP)
+	{
+		topicNameToCommand[currentlySelectedTopicName].velLeftWheel = 0;
+		topicNameToCommand[currentlySelectedTopicName].velRightWheel = 0;
+	}
+    else if(type == SmoresControllerWindow::JOINT_A1_P)
+    {
+        topicNameToCommand[currentlySelectedTopicName].angle1 += 0.1;
+    }
+    else if(type == SmoresControllerWindow::JOINT_A1_M)
+    {
+        topicNameToCommand[currentlySelectedTopicName].angle1 -= 0.1;
+    }
+    else if(type == SmoresControllerWindow::JOINT_A2_P)
+    {
+        topicNameToCommand[currentlySelectedTopicName].angle2 += 0.1;
+    }
+    else if(type == SmoresControllerWindow::JOINT_A2_M)
+    {
+        topicNameToCommand[currentlySelectedTopicName].angle2 -= 0.1;
+    }
+    else if(type == SmoresControllerWindow::JOINT_A1_TOGGLE)
+    {
+        topicNameToCommand[currentlySelectedTopicName].doRotate = !topicNameToCommand[currentlySelectedTopicName].doRotate;
+    }
+    else if(type == SmoresControllerWindow::JOINT_A2_TOGGLE)
+    {
+        topicNameToCommand[currentlySelectedTopicName].doSwipeUD = !topicNameToCommand[currentlySelectedTopicName].doSwipeUD;
+    }
+    command_message::msgs::CommandMessage msg;
+	msg.add_jointgaittablestatus(true);
+	msg.add_jointgaittablestatus(true);
+	msg.add_jointgaittablestatus(true);
+	msg.add_jointgaittablestatus(true);
+	
+	msg.add_jointgaittable(0);
+	msg.add_jointgaittable(0);
+	msg.add_jointgaittable(0);
+	msg.add_jointgaittable(0);
+    msg.set_messagetype(4);
+
+    msg.set_jointgaittable(0, topicNameToCommand[currentlySelectedTopicName].angle1);
+    msg.set_jointgaittable(1, topicNameToCommand[currentlySelectedTopicName].velLeftWheel);
+    msg.set_jointgaittable(2, topicNameToCommand[currentlySelectedTopicName].velRightWheel);
+    msg.set_jointgaittable(3, topicNameToCommand[currentlySelectedTopicName].angle2);
+
+	node->Publish<command_message::msgs::CommandMessage>(currentlySelectedTopicName, msg);
+    std::printf("Topic: %s\tJ1: %f\tJ2: %f\tLW: %f\tRW:%f\n", 
+				topicNameToCommand[currentlySelectedTopicName].topicName.c_str(),
+				topicNameToCommand[currentlySelectedTopicName].angle1, 
+				topicNameToCommand[currentlySelectedTopicName].angle2, 
+				topicNameToCommand[currentlySelectedTopicName].velLeftWheel, 
+				topicNameToCommand[currentlySelectedTopicName].velRightWheel);
 }
 
 void GLViewerThread(int argc, char** argv) {
@@ -119,45 +248,52 @@ void SwipeThread() {
 	msg.add_jointgaittable(0);
 	msg.add_jointgaittable(0);
 	
-	bool movingUp = false;
 	while(true)
 	{
-		if(doSwippingUD)
+		for(std::map<std::string, SMOREControlCommand>::iterator it = topicNameToCommand.begin(); it != topicNameToCommand.end(); ++it)
 		{
-			if(movingUp)
+			if(it->second.doSwipeUD)
 			{
-				joint2 += 0.05;
-				if(joint2 > 1.5)
-					movingUp = false;
+				if(it->second.movingUp)
+				{
+					it->second.angle2 += 0.025;
+					if(it->second.angle2 > 1.5)
+						it->second.movingUp = false;
+				}
+				else
+				{
+					it->second.angle2 -= 0.025;
+					if(it->second.angle2 < 0.35)
+						it->second.movingUp = true;
+				}
 			}
-			else
+			if(it->second.doRotate)
 			{
-				joint2 -= 0.05;
-				if(joint2 < 0.35)
-					movingUp = true;
+				it->second.angle1 += 0.01;
 			}
+			msg.set_messagetype(4);
+			msg.set_jointgaittable(0, it->second.angle1); 
+			msg.set_jointgaittable(1, it->second.velLeftWheel);
+			msg.set_jointgaittable(2, it->second.velRightWheel);
+			msg.set_jointgaittable(3, it->second.angle2);
+			if(it->second.doSwipeUD || it->second.doRotate)
+				node->Publish<command_message::msgs::CommandMessage>(it->first, msg);
+// 			pub->Publish(msg);
 		}
-		if(doRotate)
-		{
-			joint1 += 0.01;
-		}
-		msg.set_messagetype(4);
-		msg.set_jointgaittable(0, joint1); 
-		msg.set_jointgaittable(1, leftWheel);
-		msg.set_jointgaittable(2, rightWheel);
-		msg.set_jointgaittable(3, joint2);
-		if(doSwippingUD || doRotate)
-			pub->Publish(msg);
 		boost::this_thread::sleep(boost::posix_time::millisec(100));
 	}
 }
 
 void SmoresControlGUI(int argc, char **argv)
-{
+{   
     QApplication app(argc, argv);
-    SmoresControllerWindow window;
-    window.show();
-    app.exec();
+	SmoresControllerWindow window;
+	gui = &window;
+    // event handler setup
+    window.btnCalled.connect(boost::bind(GUIButtonCallback, _1));
+
+    window.show();	
+	app.exec();
 }
 
 int main(int argc, char **argv) {
@@ -166,11 +302,10 @@ int main(int argc, char **argv) {
     gazebo::load(argc, argv);
     gazebo::run();
 
-
     gazebo::physics::WorldPtr worldPtr;
 
     // create our node for communication
-    gazebo::transport::NodePtr node(new gazebo::transport::Node());
+    node = gazebo::transport::NodePtr(new gazebo::transport::Node());
     node->Init("SMORES6Uriah");
 
 	// publish to a Gazebo topic
@@ -186,7 +321,7 @@ int main(int argc, char **argv) {
     std::string lidarName = "/gazebo/default/SMORES6Uriah/SMORES_LIDAR/model/LIDAR_sensor/scan";
     gazebo::transport::SubscriberPtr lidarSub = node->Subscribe<gazebo::msgs::LaserScanStamped>(lidarName, lidarCallback);
 	
-	gazebo::transport::PublisherPtr worldMessagePublisher = node->Advertise<command_message::msgs::WorldMessage>("/gazebo/default/SMORES_WorldMessage");
+    worldMessagePublisher = node->Advertise<command_message::msgs::WorldMessage>("/gazebo/default/SMORES_WorldMessage");
 	gazebo::transport::SubscriberPtr worldMessageSubscriber = node->Subscribe<command_message::msgs::WorldMessage>("/gazebo/default/SMORES_WorldStatus", SMORESWorldStatusCallback);
         
     std::cout << "getting in to the loop..." << std::endl;
@@ -282,15 +417,10 @@ int main(int argc, char **argv) {
 			msg.set_jointgaittable(2, rightWheel);
 			msg.set_jointgaittable(3, joint2);
 	
-			pub->Publish(msg);
+			node->Publish<command_message::msgs::CommandMessage>(currentlySelectedTopicName, msg);
 			std::printf("J1: %f\tJ2: %f\tLW: %f\tRW:%f\n", joint1, joint2, leftWheel, rightWheel);
 		}
-		
-		command_message::msgs::WorldMessage worldMsg;
-		worldMsg.set_messagetype(1);
-		worldMessagePublisher->Publish(worldMsg);
-		std::cout << "Published" << std::endl;
-		
+				
 		gazebo::common::Time::MSleep(100);
     }
     glViewer->~GLViewer();
